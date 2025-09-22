d40a4fd95702749ab2271b3d56a07bc554c29b8e
kartonobinsaleh <kartono.saleh@gmail.com>
2025-09-22 07:31:45 +0700

    setup user interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,147 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (title, content) => {
+    console.log(title);
+    console.log(content);
+    setNotes([]);
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+
+  const getNoteById = (id) => {
+    console.log(id);
+  };
+
+  const updateNote = (id, newTitle, newContent) => {
+    console.log(id);
+    console.log(newTitle);
+    console.log(newContent);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <main className="min-h-screen flex flex-col mt-24 items-center">
+        <NoteForm onAddNote={addNote} />
+        <NoteList
+          notes={notes}
+          onDelete={handleDelete}
+          onUpdate={updateNote}
+          onGetById={getNoteById}
+        />
+      </main>
     </>
   );
 }
 
 export default App;
+
+// ================== Komponen ==================
+
+const Navbar = () => {
+  return (
+    <nav className="w-full fixed top-0 flex justify-center bg-white shadow">
+      <div className="flex justify-between px-5 py-5 container">
+        <img src="/logo.svg" alt="Logo" />
+      </div>
+    </nav>
+  );
+};
+
+const NoteForm = ({ onAddNote }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddNote(title, content);
+    setTitle("");
+    setContent("");
+  };
+
+  return (
+    <section className="container max-w-xl px-5 mb-8">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Title"
+          className="rounded-sm outline outline-gray-400 p-3"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          placeholder="Content"
+          className="resize-y min-h-14 rounded-sm outline outline-gray-400 p-3"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white font-semibold rounded-lg py-3"
+        >
+          Add note
+        </button>
+      </form>
+    </section>
+  );
+};
+
+const NoteItem = ({ note, onDelete, onUpdate }) => {
+  console.log(note);
+  console.log(onDelete);
+  console.log(onUpdate);
+
+  return (
+    <div className="rounded-lg shadow-md bg-white w-[300px] p-5">
+      <p className="font-medium text-xl">{note.title}</p>
+      <p className="text-sm text-gray-500">
+        ~{showFormattedDate(note.createAt)}
+      </p>
+      <p className="mt-2">{note.content}</p>
+      <div className="mt-4 flex gap-2">
+        <button className="bg-yellow-500 text-white px-3 py-1 rounded">
+          Edit
+        </button>
+        <button className="bg-red-500 text-white px-3 py-1 rounded">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const NoteList = ({ notes }) => {
+  return (
+    <section className="container py-8">
+      <h2 className="inline-flex items-center gap-2 text-2xl font-medium mb-6">
+        <img src="/note.svg" alt="note icon" className="w-8 h-8" />
+        Notes
+      </h2>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {notes.length > 0 ? (
+          notes.map((note) => <NoteItem key={note.id} note={note} />)
+        ) : (
+          <h1>Data Kosong</h1>
+        )}
+      </div>
+    </section>
+  );
+};
+
+// helper
+const showFormattedDate = (date) => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    weekday: "long",
+    day: "numeric",
+  };
+  return new Date(date).toLocaleDateString("id-ID", options);
+};
