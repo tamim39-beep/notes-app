e3307c78d30668974ade76a0bedd75a3626b29fa
tammim <tammimalghozi@gmail.com>
2025-09-23 11:21:30 +0700

    menambahkan koneksi database

---
+++ backend/src/config/db.js
@@ -0,0 +1,19 @@
+import myaql2 from "mysql2/promise";
+
+const pool = mysql2.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "notes_app",
+});
+
+const testConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connected successfully.");
+    connection.release();
+  } catch (error) {
+    console.error("Database error:", error);
+    throw error;
+  }
+};
