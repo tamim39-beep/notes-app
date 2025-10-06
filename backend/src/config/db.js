fccbb8021695d88c6f5ac29060fc5051dadc5795
tammim <tammimalghozi@gmail.com>
2025-10-06 11:43:38 +0700

    setup baru

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,13 @@
 import mysql2 from "mysql2/promise";
 
 export const pool = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "notes_app",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12801496",
+  password: " 1U1d9rpkJk",
+  database: "sql12801496",
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
 });
 
 export const testConnection = async () => {
